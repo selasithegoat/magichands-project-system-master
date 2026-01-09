cef9bc7d068f71ce57ad8c50d12107d22ae213bc
selasithegoat <enochdoe355@gmail.com>
2026-01-09 08:36:04 +0000

    created the server beginning files and set up db config, http constants and .env

---
+++ server/src/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+// Load env vars
+dotenv.config();
+
+// Connect to database
+connectDB();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+// Base Route
+app.get("/", (req, res) => {
+  res.send("MagicHands Server is Running");
+});
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
